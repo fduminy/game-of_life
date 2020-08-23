0f715e84bd2d7d7328e4a6ebf55d363fac3f16b0
Fabien DUMINY <fduminy@jnode.org>
2020-08-23 16:42:18 +0200

    Optimize DefaultRule while counting alive neighbours.

--- src/main/java/fr/duminy/game/life/DefaultRule.java
+++ src/main/java/fr/duminy/game/life/DefaultRule.java
@@ -1,21 +1,32 @@
 package fr.duminy.game.life;
 
+import static java.lang.Math.max;
+
 public class DefaultRule implements Rule {
+    static final int NB_ALIVE_NEIGHBOURS_TO_BORN = 3;
+    static final int MIN_NB_ALIVE_NEIGHBOURS_TO_SURVIVE = 2;
+    static final int MAX_NB_ALIVE_NEIGHBOURS_TO_SURVIVE = 3;
+    static final int NB_ALIVE_NEIGHBOURS_TO_CHECK = max(NB_ALIVE_NEIGHBOURS_TO_BORN, MAX_NB_ALIVE_NEIGHBOURS_TO_SURVIVE);
+
     @Override
     public boolean evolve(CellView view) {
-        int count = 0;
+        int nbNeighbours = 0;
+        out:
         for (int deltaY = -1; deltaY < 2; deltaY++) {
             for (int deltaX = -1; deltaX < 2; deltaX++) {
                 if (!((deltaX == 0) && (deltaY == 0)) && view.isAlive(deltaX, deltaY)) {
-                    count++;
+                    nbNeighbours++;
+                    if (nbNeighbours > NB_ALIVE_NEIGHBOURS_TO_CHECK) {
+                        break out;
+                    }
                 }
             }
         }
 
         boolean alive = view.isAlive(0, 0);
-        if (alive && ((count < 2) || (count > 3))) {
+        if (alive && ((nbNeighbours < MIN_NB_ALIVE_NEIGHBOURS_TO_SURVIVE) || (nbNeighbours > MAX_NB_ALIVE_NEIGHBOURS_TO_SURVIVE))) {
             alive = false;
-        } else if (!alive && (count == 3)) {
+        } else if (!alive && (nbNeighbours == NB_ALIVE_NEIGHBOURS_TO_BORN)) {
             alive = true;
         }
 
--- src/test/java/fr/duminy/game/life/DefaultRuleTest.java
+++ src/test/java/fr/duminy/game/life/DefaultRuleTest.java
@@ -5,35 +5,66 @@ import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 import net.jqwik.api.constraints.Size;
 
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import static fr.duminy.game.life.DefaultCellViewTest.Position.CENTER;
+import static fr.duminy.game.life.DefaultRule.MAX_NB_ALIVE_NEIGHBOURS_TO_SURVIVE;
+import static fr.duminy.game.life.DefaultRule.MIN_NB_ALIVE_NEIGHBOURS_TO_SURVIVE;
+import static fr.duminy.game.life.DefaultRule.NB_ALIVE_NEIGHBOURS_TO_BORN;
+import static fr.duminy.game.life.DefaultRule.NB_ALIVE_NEIGHBOURS_TO_CHECK;
+import static java.util.Comparator.comparing;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("DefaultAnnotationParam")
 class DefaultRuleTest {
+    private static final Comparator<Position> COMPARATOR = comparing(Position::ordinal);
+
     @Property
     void a_cell_with_2_or_3_neighbours_survives(@ForAll @Size(min = 0, max = 8) Set<Position> positions) {
-        assumeFalse(positions.contains(CENTER));
-        CellView view = mock(CellView.class);
-        when(view.isAlive(CENTER.getDeltaX(), CENTER.getDeltaY())).thenReturn(true);
-        for (Position position : positions) {
-            when(view.isAlive(position.getDeltaX(), position.getDeltaY())).thenReturn(true);
-        }
-        assertThat(new DefaultRule().evolve(view)).isEqualTo((positions.size() == 2) || (positions.size() == 3));
+        testEvolve(positions, true, nbAliveNeighbours -> (nbAliveNeighbours >= MIN_NB_ALIVE_NEIGHBOURS_TO_SURVIVE) && (nbAliveNeighbours <= MAX_NB_ALIVE_NEIGHBOURS_TO_SURVIVE));
     }
 
     @Property
     void a_cell_with_3_neighbours_borns(@ForAll @Size(min = 0, max = 8) Set<Position> positions) {
+        testEvolve(positions, false, nbAliveNeighbours -> nbAliveNeighbours == NB_ALIVE_NEIGHBOURS_TO_BORN);
+    }
+
+    private void testEvolve(Set<Position> positions, boolean cellIsAlive, Predicate<Integer> conditionToBeAlive) {
         assumeFalse(positions.contains(CENTER));
         CellView view = mock(CellView.class);
-        when(view.isAlive(CENTER.getDeltaX(), CENTER.getDeltaY())).thenReturn(false);
+        when(view.isAlive(CENTER.getDeltaX(), CENTER.getDeltaY())).thenReturn(cellIsAlive);
         for (Position position : positions) {
             when(view.isAlive(position.getDeltaX(), position.getDeltaY())).thenReturn(true);
         }
-        assertThat(new DefaultRule().evolve(view)).isEqualTo(positions.size() == 3);
+        int nbAliveNeighbours = positions.size();
+        int expectedCallsToIsAlive = 1 + expectedCallsToIsAliveWithoutSelf(positions, nbAliveNeighbours);
+        assertAll(
+                () -> assertThat(new DefaultRule().evolve(view)).isEqualTo(conditionToBeAlive.test(nbAliveNeighbours)),
+                () -> verify(view, times(expectedCallsToIsAlive)).isAlive(anyInt(), anyInt())
+        );
+    }
+
+    private int expectedCallsToIsAliveWithoutSelf(Set<Position> positions, int nbAliveNeighbours) {
+        int expectedCallsToIsAliveWithoutSelf = 8;
+        int maxAliveNeighboursToFind = NB_ALIVE_NEIGHBOURS_TO_CHECK + 1;
+        if (nbAliveNeighbours >= maxAliveNeighboursToFind) {
+            expectedCallsToIsAliveWithoutSelf = maxAliveNeighboursToFind;
+            Optional<Position> maxPosition = positions.stream().sorted(COMPARATOR).limit(maxAliveNeighboursToFind)
+                    .max(COMPARATOR);
+            if (maxPosition.isPresent() && (maxPosition.get().ordinal() > expectedCallsToIsAliveWithoutSelf)) {
+                expectedCallsToIsAliveWithoutSelf = maxPosition.get().ordinal();
+            }
+        }
+        return expectedCallsToIsAliveWithoutSelf;
     }
 }
